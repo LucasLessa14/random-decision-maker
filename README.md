50c4e21e5a705be65c769f563ce82b40de9ba543
Lucas Lessa <lucaslessa14@hotmail.com>
2020-10-20 18:43:18 -0300

    Numeros aleatorios com intervalo variavel

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
-# random-decision-maker
+# Tomador de Decisões Aleatórias
 
-## Project setup
-```
-npm install
-```
+[imagem aqui](./public/build.png)
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+## Tecnologias
+---
+- Node.js
+- VueJS
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## Sobre o projeto
+---
+No mundo dos negócios, basta você tomar poucas decisões certas para alcançar o sucesso. No entanto, perdemos muito tempo com decisões que não geram tanto impacto. Esse software toma decisões não importante por você baseado em números aleatório.
 
-### Lints and fixes files
-```
-npm run lint
-```
+Esse projeto foi inspirado em um dos projetos da lista de [80 app-ideias](https://github.com/florinpop17/app-ideas)
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Histórias de usuários
+---
+- [x] Usuário define um número máximo e mínimo para receber seus valores aleatórios
+- [x] Usuário pressiona o botão `gerar` para gerar os valores aleatórios 
+
+### Licença
+---
+Esse projeto está sob a licença MIT. Veja o arquivo LICENSE para mais detalhes.
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,131 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+      <p>Gerador de números aleatório para tomadas de decisões ao acaso</p>
+    </div>
+
+    <div class="form">
+      <label for="max-number">Digite o maior número</label>
+      <input type="number" name="max-number" id="max-number" placeholder="Maior número" v-model="max">
+      <label for="min-number">Digite o menor número</label>
+      <input type="number" name="min-number" id="min-number" placeholder="Menor número" v-model="min">
+      <button class="btn" @click="gerar">Gerar</button>
+    </div>
+
+    <div v-if="showResult" class="result">
+      <p>Número aleatório gerado: {{ randomNumber }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      max: 5,
+      min: 0,
+      randomNumber: 0,
+      showResult: false
+    }
+  },
+  methods: {
+    gerar() {
+      if (!this.showResult) this.showResult = true;
+      this.randomNumber = parseInt(this.min) + Math.floor((this.max - this.min + 1) * Math.random());
+    }
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #F7F6F3;
+  font-family: 'Josefin Sans', sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+
+.header {
+  width: 80%;
+  max-width: 496px;
+  font-size: 24px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-bottom: 32px;
+}
+
+.form {
+  width: 90%;
+  max-width: 496px;
+  background-color: #fff;
+  border-radius: 20px;
+  padding: 32px 16px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+}
+
+label {
+  margin-top: 32px;
+  margin-bottom: 16px;
+}
+
+input[type=number] {
+  width: 100%;
+  height: 48px;
+  border-radius: 8px;
+  border: 1px solid #722196;
+  padding: 0 8px;
+}
+
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
+
+input[type=number]:focus {
+  outline: none;
+  box-shadow: none;
+}
+
+.btn {
+  background: linear-gradient(135deg, #500870 0%, #A31564 100%);
+  border-radius: 8px;
+  width: 100%;
+  height: 48px;
+  border: none;
+  color: #fff;
+  font-size: 24px;
+  margin-top: 32px;
+}
+
+.btn:focus {
+  outline: none;
+  box-shadow: none;
+}
+
+.result {
+  margin-top: 32px;
 }
 </style>
